a3fd36c87f5601b7ae21e3c8695a46a3432b1b81
Jakob Selja <jakob.selja@DETNSW>
2019-12-02 14:56:20 +1100

    Add input variable

--- run.py
+++ run.py
@@ -1,7 +1,21 @@
-# py-textbased-adventure
-# A dynamic text adventure engine by Jakob Selja.
 
+   ## py-textbased-adventure ##
+
+# A dynamic text adventure engine by Jakob Selja.
 # Project started on 2/12/2019
 
+import os
+
+uInput = ''
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    clear()
+    print()
+
+    uInput = input('  > ')
+
+    if uInput == 'hello':
+        print("Message recieved.")
